56671a3cb8faf15d2b52ba0e5164118a521b517d
yjingj <yjingj@gmail.com>
2022-07-22 20:04:54 -0400

    update Mstep

--- bin/Mstep.r
+++ bin/Mstep.r
@@ -2,137 +2,139 @@ library(optimx)
 Sys.setlocale('LC_ALL', 'C')
 options(stringsAsFactors=F)
 
+# Package names
+packages <- c("data.table", "tidyverse", "optimx")
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
+
+#################################
 ptm <- proc.time()
 
-######## Need to pass args(hypfile, paramfile, k, hypcurrent_file) from bash
+######## Read input arguments
 args <- commandArgs(TRUE)
-rv = args[[1]]
-#the number of iterition 
-k = as.numeric(args[[2]])
-#hyper.current
-hypcurrent_file = args[[3]]
-#Annotation file <- [SNPs,c(SNPname, gamma, beta, annotation1, annotation2, annotation3...)]
-Annofile = args[[4]]
-print(hypcurrent_file)
-wkDir = args[[5]]
-abgamma = as.numeric(args[[6]])
-########## Load beta and gamma 
-Annodata = read.table(Annofile, sep="\t", header=FALSE)
-#Number of annotation files
-Anum = ncol(Annodata) - 3
-temp_col_names <- c("SNP","gamma", "beta")
-for(i in 1:Anum){
-	temp_col_names <- c(temp_col_names, 
-	paste("Annotation", i, sep = "_"))
-}
-colnames(Annodata) <- temp_col_names
-a_gamma = abgamma
-b_gamma = abgamma
+hypfile=args[[1]]
+k = as.numeric(args[[2]]) #the number of iterition
+a_gamma = as.numeric(args[[3]])
+b_gamma = as.numeric(args[[4]])
+gwas_n = as.numeric(args[[5]]) # GWAS sample size
+hypcurrent_file = args[[6]] # hyper.current
+# sum(gamma) and sum(gamma * beta^2) are saved in hypfile
+param_file = args[[7]] # Annotation data and gamma
+EM_result_file = args[[8]] # Save EM_result_file
 
-########### Update hyper parameter values
+########### Read prior hyper parameter values
 #rv is phenotype variance, rv = mean(hypdata[, "rv"])
-tau = 1.0 / as.numeric(rv)
+# hypcurrent_file = "/home/jyang/ResearchProjects/BFGWAS_QUANT_Test/Test_Data/hypval_10anno.txt"
 prehyp <- read.table(hypcurrent_file, header=F)
 print("hyper parameter values before MCMC: ")
 print(prehyp)
+avec_old = str_split(prehyp[1, 2], ",") %>% unlist() %>% as.numeric()
+tau_beta_old = as.numeric(prehyp[2, 2])
 
-a_old = prehyp[,1]
-a_variance = prehyp[,3]
-inverse_a_Var_matrix = diag(1/a_variance)
+########## Read hyptemp file
+# hypfile = "/home/jyang/ResearchProjects/BFGWAS_QUANT_Test/Test_wkdir/output/WGS_1898_samples_CHR_19_43862455_44744108.hyptemp"
+hyp_dt = fread(hypfile, sep = "\t", header = TRUE)
+sum_gamma = sum(hyp_dt$sum_gamma)
+sum_beta2 = sum(hyp_dt$sum_beta2)
 
-#### updating hyper a vector and b vector
-hypmat <- NULL
-
-if (nrow(Annodata[Annodata$gamma > 0,]) > 0){
-  Annodata = Annodata[Annodata$gamma > 0,]
+########## Read annotation data
+# param_file="/home/jyang/ResearchProjects/BFGWAS_QUANT_Test/Test_wkdir/output/WGS_1898_samples_CHR_19_43862455_44744108.paramtemp"
+param_dt = fread(param_file, sep="\t", header=TRUE)
+gamma_temp = param_dt$Pi
+split_anno <- function(x){
+  xvec = str_split(x, ",") %>% unlist()%>% t() %>% data.frame()
+  return(xvec)
 }
-beta_temp = Annodata[, "beta"]
-gamma_temp = Annodata[,"gamma"]
-A_temp = cbind(rep(1,nrow(Annodata)),Annodata[,-c(1:3)])
-A_temp = as.matrix(A_temp)
+Anno_df = map_dfr(param_dt$Anno, split_anno)
+Anum = ncol(Anno_df)
+A_temp = data.frame(intercept = 1, Anno_df) %>% unlist() %>% as.numeric() %>% matrix(nrow = nrow(param_dt))
+gamma_A = gamma_temp * A_temp
 
 ####################################################
+####### Solve for a_vec
 a_fn <- function(a) {
-  asum = sum(gamma_temp * (A_temp %*% a) - log(1 + exp(A_temp %*% a))) - sum(1/2 * t(a) %*% (inverse_a_Var_matrix * a)) 
-  asum = as.vector(asum)
+  Ata = A_temp %*% a
+  asum = sum(gamma_temp * Ata - log(1 + exp(Ata))) - 0.5 * sum(a * a)
   return(-asum)
 }
 
 a_gr <- function(a) {
-  a_gr_sum = apply(gamma_temp * A_temp - as.vector(1/(1 + exp(-A_temp %*% a))) * A_temp,2,sum) - apply(inverse_a_Var_matrix * a, 2, sum)
-  a_gr_sum = as.vector(a_gr_sum)
-  return(-a_gr_sum)
+  Ata = A_temp %*% a
+  a_gr_sum = apply( (gamma_A - as.vector(1/(1 + exp(-Ata))) * A_temp) , 2, sum) - a
+  return( as.vector(-a_gr_sum) )
 }
 
 a_Hess <- function(a) {
-  a_Hess_sum = matrix(0,nrow(inverse_a_Var_matrix),ncol(inverse_a_Var_matrix))
+  a_num = length(a)
+  exp_mAta = exp(-A_temp %*% a)
+  a_Hess_sum = matrix(0, a_num, a_num)
   for (i in 1:nrow(A_temp)) {
-    a_Hess_value = as.numeric(-exp(-A_temp[i,] %*% a)/(1 + exp(-A_temp[i,] %*% a)) ^ 2) * outer(A_temp[i,],A_temp[i,]) 
-    a_Hess_sum = a_Hess_sum + a_Hess_value
+    a_Hess_sum = a_Hess_sum + (exp_mAta[i] / ((1 + exp_mAta[i])^2) ) * outer(A_temp[i,], A_temp[i,])
   }
-  a_Hess_sum = a_Hess_sum - inverse_a_Var_matrix
-  a_Hess_sum = as.matrix(a_Hess_sum)
-  return(-(a_Hess_sum))
+  a_Hess_sum = a_Hess_sum + diag(rep(1, a_num))
+  return( as.matrix(a_Hess_sum))
 }
 
-a_new_Variance = diag(solve(a_Hess(a_old)))
-#e = try( optimx(a_old, a_fn, method='L-BFGS-B', gr = a_gr, hess = a_Hess, upper = c(-12, rep(10,Anum)))[1:nrow(inverse_a_Var_matrix)], TRUE)
-#if (class(e) != "try-error"){
-#  a_temp = optimx(a_old, a_fn, method='L-BFGS-B', gr = a_gr, hess = a_Hess, upper = c(-12, rep(10,Anum)))[1:nrow(inverse_a_Var_matrix)]
-#}else{
-  a_old = upper = c(-40, rep(0,Anum))
-  a_temp = optimx(a_old, a_fn, method='L-BFGS-B', gr = a_gr, hess = a_Hess, upper = c(-12, rep(10,Anum)))[1:nrow(inverse_a_Var_matrix)]
-#}
+a_temp = optimx(avec_old, fn = a_fn, method='L-BFGS-B', gr = a_gr, hess = a_Hess,
+                upper = c(avec_old[1], rep(10, Anum)), lower = c(avec_old[1], rep(-10, Anum)) ) [1:length(avec_old)]
+
 ####################################################
-Est_sigma2 <- function(sigma2, m, tau, a, b){
-  sigma2_hat = (sum(sigma2 * m) * tau + 2 * b) / (sum(m) + 2 * (a + 1))
-  return(sigma2_hat)
+# gwas_n = 2453; a_gamma = 1.01; b_gamma = 1
+Est_tau_beta <- function(sum_gamma, sum_beta2, gwas_n, a, b){
+  tau_beta_hat =  ( sum_gamma + 2 * (a - 1)) / ( gwas_n * sum_beta2 + 2 * b)
+  return(tau_beta_hat)
 }
 
-CI_fish_sigma2 <- function(sigma2, m, tau, a, b){
-  sigma2_hat = Est_sigma2(sigma2, m, tau, a, b)
-  if( (sum(m) * tau - sum(m)/2 - (a+1) + 2*b/sigma2_hat) < 0){
-    se_sigma2=0
+CI_fish_tau_beta <- function(sum_gamma, sum_beta2, gwas_n, a, b){
+  tau_beta_hat =  ( sum_gamma + 2 * (a - 1)) / ( gwas_n * sum_beta2 + 2 * b)
+  temp = 0.5 * sum_gamma + (a - 1)
+  if( temp  < 0){
+    se_tau_beta = 0
   }else{
-    se_sigma2 = sigma2_hat * sqrt(1/(sum(m) * tau - sum(m)/2 - (a+1) + 2*b/sigma2_hat))
+    se_tau_beta = tau_beta_hat / sqrt(temp)
   }
-  return(c(sigma2_hat, se_sigma2))
+  return(c(tau_beta_hat, se_tau_beta))
 }
 
-sigma2_temp = CI_fish_sigma2(gamma_temp, beta_temp, tau, a_gamma, b_gamma)
+tau_beta_temp = CI_fish_tau_beta(sum_gamma, sum_beta2, gwas_n, a_gamma, b_gamma)
+print("Estimates for tau_beta, and tau_beta_SE: ")
+print(tau_beta_temp)
 
-b_temp = rep(sigma2_temp[1], length(a_temp))
-b_new_Variance = rep(sigma2_temp[2], length(a_temp))
 #####################################################
-#hypcurrent <- c(a_temp, b_temp)
-hypmat <- data.frame(as.vector(t(a_temp)), as.vector(t(b_temp)), a_new_Variance, b_new_Variance)
+hypmat <- data.table(`#hyper_parameter` = c("a", "tau_beta"), value = c(paste(a_temp, collapse = ",") , tau_beta_temp[1]))
 
 ########## Write out updated hyper parameter values
-colnames(hypmat) <- c("#a", "b", "a_variance", "b_variance")
 print("hyper parameter values updates after MCMC: ")
 print(hypmat)
+# hypcurrent_file="/home/jyang/ResearchProjects/BFGWAS_QUANT_Test/Test_wkdir/hyper.current.txt"
 write.table(format(hypmat, scientific=TRUE), 
-            file=hypcurrent_file, 
+            file=hypcurrent_file,
             quote = FALSE, sep = "\t", row.names=FALSE, col.names=TRUE)
-
-write.table(Annodata,
-            file=paste0(wkDir , "/Anno_data", k, ".txt"), 
-            quote = FALSE, sep = "\t", row.names=FALSE, col.names=TRUE)
-
-write.table(format(hypmat, scientific=TRUE), 
-            file=paste0(hypcurrent_file, "-", k), 
-            quote = FALSE, sep = "\t", row.names=FALSE, col.names=TRUE)
-
-
-#### Summarize log-likelihood
-#loglike_total = sum(hypdata$loglike)
 
 ########## Write out updated hyper parameter values and se to EM_result_file
-#hypcurrent = c(pve, loglike_total, hypcurrent)
-#hypcurrent <- format(hypcurrent, scientific = TRUE)
-#print("write to hypcurrent file with hyper parameter values after MCMC: ")
-#print(c(k, hypcurrent))
-#write.table(matrix(c(k, hypcurrent), nrow=1), file = EM_result_file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE, append=TRUE)
+R2 = sum(hyp_dt$r2)
+loglike = sum(hyp_dt$log_post_likelihood)
+print(paste("Sum PIP = ", sum_gamma))
+print(paste("Regression R2 = ", R2))
+print(paste("Posterior log likelihood = ",  loglike))
+
+# EM_result_file="/home/jyang/ResearchProjects/BFGWAS_QUANT_Test/Test_wkdir/EM_result.txt"
+if(k==0){
+  write.table(data.frame(EM_iteration = k, R2 = R2, Loglike = loglike,
+                        tau_beta = tau_beta_temp[1], avec = paste(a_temp, collapse = ",")),
+              file = EM_result_file,
+              sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE, append=FALSE)
+}else{
+  write.table(data.frame(EM_iteration = k, R2 = R2, Loglike = loglike,
+                        tau_beta = tau_beta_temp[1], avec = paste(a_temp, collapse = ",")),
+              file = EM_result_file,
+              sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE, append=TRUE)
+}
 
 print("EM step time cost (in minutes) : ")
 print((proc.time() - ptm)/60)
